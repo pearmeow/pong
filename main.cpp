a74b82c4c5c83eead6e82b038901221eccdcd009
Perry Huang <perryhuang2015@gmail.com>
2025-10-02 17:46:51 -0400

    feat: add comment at top of main

--- main.cpp
+++ main.cpp
@@ -1,7 +1,11 @@
 /**
- * @file
- * @author Perry Huang
- * @date 2025-
+ * Author: Perry Huang
+ * Assignment: Pong Clone
+ * Date due: 2025-10-13, 11:59pm
+ * I pledge that I have completed this assignment without
+ * collaborating with anyone else, in conformance with the
+ * NYU School of Engineering Policies and Procedures on
+ * Academic Misconduct.
  */
 
 #include "raylib.h"
