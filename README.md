8468552b6916c82e3c63c5e3f22f257d33ab2090
Perry Huang <perryhuang2015@gmail.com>
2025-10-02 17:19:53 -0400

    feat: add goals in readme and initial files

---
+++ README.md
@@ -0,0 +1,19 @@
+pong
+----
+
+A pong game made with raylib
+
+Features
+--------
+
+- [ ] Two paddles
+- [ ] Movable paddles with WASD and arrow keys for left and right respectively
+- [ ] Ball that starts in middle
+- [ ] Ball bounces off top and bottom walls
+- [ ] Ball bounces off paddles
+- [ ] Game over when ball hits right and left walls
+- [ ] Singleplayer mode when t is pressed
+- [ ] Up and down motion for right paddle when singleplayer
+- [ ] Generate multiple balls when number is pressed
+- [ ] Theme in the background
+- [ ] Game over screen
---
+++ main.cpp
@@ -0,0 +1,73 @@
+/**
+ * @file
+ * @author Perry Huang
+ * @date 2025-
+ */
+
+#include "raylib.h"
+
+// Enums
+enum AppStatus { TERMINATED, RUNNING };
+enum Direction { LEFT, RIGHT };
+
+// Global Constants
+constexpr int SCREEN_WIDTH = 800, SCREEN_HEIGHT = 450, FPS = 60, SIDES = 4;
+
+constexpr Vector2 ORIGIN = {SCREEN_WIDTH / 2.0, SCREEN_HEIGHT / 2.0};
+
+// Global Variables
+AppStatus gAppStatus = RUNNING;
+float gAngle = 0.0f;
+float gScaleFactor = 100;
+Direction gDirection = LEFT;
+Vector2 gPosition = {0.0f, 0.0f};
+
+// Function Declarations
+void initialise();
+void processInput();
+void update();
+void render();
+void shutdown();
+
+// Function Definitions
+void initialise() {
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Hello raylib!");
+
+    SetTargetFPS(FPS);
+}
+
+void processInput() {
+    if (WindowShouldClose()) gAppStatus = TERMINATED;
+}
+
+void update() {
+}
+
+void render() {
+    BeginDrawing();
+
+    ClearBackground(RAYWHITE);
+
+    // Draw something
+    // DrawPoly(gPosition, SIDES, gScaleFactor, gAngle, ORANGE);
+
+    EndDrawing();
+}
+
+void shutdown() {
+    CloseWindow();  // Close window and OpenGL context
+}
+
+int main(void) {
+    initialise();
+
+    while (gAppStatus == RUNNING) {
+        processInput();
+        update();
+        render();
+    }
+
+    shutdown();
+
+    return 0;
+}
